c20c4949711f34acfc8cb4ee5b2c7729949daa0b
iqzar <iqraa7191@gmail.com>
2025-02-08 15:12:38 +0500

    new

--- src/sanity/lib/imageUrlHelper.ts
+++
@@ -1,8 +0,0 @@
-import imageUrlBuilder from '@sanity/image-url';
-import { client } from './client'; // Ensure you have your Sanity client set up
-
-const builder = imageUrlBuilder(client);
-
-export const urlFor = (source: any) => {
-  return builder.image(source).url(); // Returns the full image URL
-};
